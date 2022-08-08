3b3e00411e55f29e86522315dda28368971e717b
David Brockbank <broda677@student.otago.ac.nz>
2022-08-09 11:49:05 +1200

    bout to go mobile first

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 
 function createBirdElement(bird) {
     const birdArticle = document.createElement('article');
-    birdArticle.setAttribute('class', 'birdArticle')
+    birdArticle.setAttribute('class', 'birdArticle');
     birdArticle.append(newImgElement('bird-main-img', bird));
+    birdArticle.append(newBirdHeaderElement('bird-header', bird));
     birdArticle.append(newBirdTextElement('bird-textbox', bird));
 
 
@@ -17,28 +18,56 @@ function createBirdElement(bird) {
     document.querySelector('#bird-content-container').prepend(birdArticle);
 }
 
+function newBirdHeaderElement(className, data) {
+    const header = document.createElement('h3');
+    header.innerHTML = data.english_name;
+    header.setAttribute('class', 'article-header');
+
+    return header;
+}
+
+
 function newBirdTextElement(className, data) {
     const textBox = document.createElement('div');
     textBox.setAttribute('class', className);
 
-    const header = document.createElement('h3');
-    header.innerHTML = data.english_name;
-    header.setAttribute('class', 'article-header');
-    textBox.append(header);
+    const keybox = document.createElement('div');
+    const valuebox = document.createElement('div');
+    keybox.setAttribute('class', 'keybox');
+    valuebox.setAttribute('class', 'valuebox');
+
+    newDescriptionKeyValue(keybox, valuebox, 'scientific_name', data);
+    newDescriptionKeyValue(keybox, valuebox, 'family', data);
+    newDescriptionKeyValue(keybox, valuebox, 'order', data);
+    newDescriptionKeyValue(keybox, valuebox, 'status', data);
+    newDescriptionKeyValue(keybox, valuebox, 'length', data);
+    newDescriptionKeyValue(keybox, valuebox, 'weight', data);
 
 
 
-
-    for (key in data) {
-        const line = document.createElement('p');
-        line.innerHTML = key.replaceAll('_', ' ') + ' ' + data[key];
-        textBox.append(line);
-
-    }
-
+    textBox.append(keybox);
+    textBox.append(valuebox);
 
 
     return textBox;
+}
+
+function newDescriptionKeyValue(keybox, valuebox, key, data) {
+    const valueLine = document.createElement('p');
+    const keyLine = document.createElement('p');
+
+    if (key === 'length' || key === 'weight') {
+        valueLine.innerHTML = data.size[key].value + ' ' + data.size[key].units;
+        keyLine.innerHTML = key[0].toUpperCase().bold() + key.replaceAll('_', ' ').substring(1).bold();
+
+    } else {
+        valueLine.innerHTML = data[key];
+        keyLine.innerHTML = key[0].toUpperCase().bold() + key.replaceAll('_', ' ').substring(1).bold();
+    }
+
+    keybox.append(keyLine);
+    valuebox.append(valueLine);
+
 }
 
 
